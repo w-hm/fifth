7b63cd1dba5e3d7a8b3d4311357a2f7a081c78ad
王淑静 <oushukusei@wsjhhhhhh.local>
2019-08-11 17:01:41 +0800

    滚动

--- hanfuhui/src/views/index.vue
+++ hanfuhui/src/views/index.vue
@@ -10,35 +10,35 @@
         <div class="classify">
             <ul class="classify_ul">     
                 <div>                  
-                    <img src="https://m.hanfugou.com/Image/icon_type_qixiong.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_qixiong.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>齐胸</span>
                 </div>    
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_beizi.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_beizi.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>褙子</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_jiaoling.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_jiaoling.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>交领襦裙</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_duijing.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_duijing.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>对襟襦裙</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_aoqun.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_aoqun.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>袄裙</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_zhoubian.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_zhoubian.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>配饰周边</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_hanyuansu.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_hanyuansu.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>汉元素</span>
                 </div>  
                  <div>
-                    <img src="https://m.hanfugou.com/Image/icon_type_more.png?v=3" alt="" style="width:0.6rem;height:0.6rem;">
+                    <img src="https://m.hanfugou.com/Image/icon_type_more.png?v=3" alt="" style="width:1rem;height:1rem;">
                     <span>全部分类</span>
                 </div>                                 
             </ul>
@@ -46,11 +46,11 @@
 
         <div class="nav_list">
             <router-link class="brand" tag="div" to="/brand">
-                <img src="https://m.hanfugou.com/Image/nav_shop.png" alt="" style="width:0.43rem;height:0.4rem;"/>
+                <img src="https://m.hanfugou.com/Image/nav_shop.png" alt="" style="width:0.65rem;height:0.6rem;"/>
                 <span>品牌馆</span>
             </router-link>
             <router-link class="two_hand" tag="div" to="/twohand">
-                <img src="https://m.hanfugou.com/Image/nav_fleas.png" alt="" style="width:0.43rem;height:0.4rem;"/>
+                <img src="https://m.hanfugou.com/Image/nav_fleas.png" alt="" style="width:0.65rem;height:0.6rem;"/>
                 <span>二手铺</span>
             </router-link>
         </div>
@@ -58,18 +58,59 @@
         <div class="today">
             <div class="title">
                 <div class="time">
-                    <img src="https://m.hanfugou.com/Image/icon_seckill_title.png" alt="" style="width:0.7rem;height:0.22rem;" />
+                    <img src="https://m.hanfugou.com/Image/icon_seckill_title.png" alt="" style="width:1.5rem;height:0.45rem;" />
                     <span class="countDown">
                         0天00时31分45秒 后开始
                     </span>
                 </div>
                 <div class="more">
                     <span>查看更多</span>
-                    <img src="https://m.hanfugou.com/Image/icon_more.png" alt="" style="width:16px;height:16px;"/>
+                    <img src="https://m.hanfugou.com/Image/icon_more.png" alt="" style="width:0.3rem;height:0.3rem;"/>
                 </div>
             </div>
-            <div class="pic"></div>
+           
+            <div class="pic" ref="picWrap">  
+                 <div class="box">
+                <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                 <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                 <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+                 <div class="content">
+                    <img src="../../public/img/yyqx.jpeg" alt="" style="width:2rem;height:2rem;">
+                    <span class="nowPrice">¥ 219.00</span>
+                    <span class="oldPrice">¥ 249.00</span>
+                </div>
+        </div> 
+            </div>
         </div>
+
+        
 
         <div class="footer">
             <router-link class="shopping" tag="div" to="/shoppingcart">
@@ -84,34 +125,102 @@
     </div>
 </template>
 <script>
+
+import BScroll from "better-scroll"
 export default {
-    name:"index"
+    name:"index",
+    data(){
+        return{
+
+        }
+    },
+    created(){
+        this.$nextTick(()=>{
+          if(!this.scroll){
+               this.scroll= new BScroll(this.$refs.picWrap,{
+                  
+           })
+           console.log(this.scroll)
+          }else{
+              this.scroll.refresh()
+          }
+        })
+    }
 }
 </script>
 <style lang="css" scoped>
    
     .title{
         width:100%;
-        margin-top:0.15rem;
+        margin-top:0.3rem;
         background-color: #fff;
         display: flex;
         justify-content: space-between;
-        padding: 0.15rem;
-    }   
+        padding: 0.3rem;
+       
+    }  
+    .today{
+         position: relative;
+    } 
     .today .time{
         display: flex;
-        font-size: 0.12rem;  
+        font-size: 0.22rem;  
         justify-content: center;
         align-items: center;
     }
+    .today .time span{
+        color: white;
+        margin-left: 0.2rem;
+        padding: 0.1rem 0.15rem;
+        border-radius: 10px;
+        background-color: #ff4466;
+    }
+    .pic{
+        display: flex;
+        margin-right: 0.3rem;
+        width: 100%;
+        overflow: hidden;
+        position: absolute;
+        left: 0;
+        top: 1.1rem;
+    }
+   .pic .box{
+       display: flex;
+   }
+    .pic .content{
+        width: 30%;
+        background-color: #fff;
+        padding:0 0.3rem 0.3rem;
+        
+        display: flex;
+        flex-direction: column;
+        
+    }
+    .pic .content span{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0.1rem 0 0 0.3rem;
+    } 
+    .pic .content .nowPrice{
+        color: red;
+    }
+    .pic .content .oldPrice{
+        text-decoration: line-through ;
+        color: grey;
+    }  
+
+
+
+
     .today .more{  
-        font-size: 0.14rem;
+        font-size: 0.26rem;
          color: grey;
     }
     .today .more span{
-        margin-right: 0.05rem;
+        margin-right: 0.2rem;
     }
-
+  
 
 
     .footer{
@@ -121,13 +230,13 @@ export default {
         bottom:0;
         color: grey;
         width: 100%;
-        height: 0.6rem;
+        height: 1.1rem;
         border-top:1px solid lightgray;                                                                                                                                                                                                                                                                                                                                                                                                        
         padding: 0.1rem 0;
         background-color: white;
     }
     .footer img{
-        margin-right: 0.05rem;
+        margin-right: 0.1rem;
     }
     .shopping{
         flex: 1;
@@ -170,7 +279,7 @@ export default {
 
     .classify{
         border-bottom:1px solid lightgray;
-        padding-bottom:0.15rem; 
+        padding-bottom:0.4rem; 
         background-color: white;
     }
     .classify_ul{
@@ -179,24 +288,24 @@ export default {
     }
     .classify_ul div{
         width: 25%;
-        height: 0.9rem;
+        height: 1.5rem;
         display: flex;
         flex-direction: column;    
         justify-content: space-between; 
         align-items: center;    
         color:grey;
-        margin-top: 0.2rem;
+        margin-top: 0.4rem;
     }
    
 
     .nav_list{
-        padding:0.1rem 0;
+        padding:0.25rem 0;
         display: flex;
         border-bottom:1px solid #f8f8f8;
         background-color: white;
     }
     .nav_list span{
-        margin-left: 0.08rem;
+        margin-left: 0.2rem;
         color:grey;
     }
     .nav_list .brand{
