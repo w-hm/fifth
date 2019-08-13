f8e7931a090a4273d8fe9d228a8b7e9c18448dd1
wangbuyuan <814892235@qq.com>
2019-08-13 15:33:32 +0800

    2222

--- hanfuhui/src/views/Brand/list/index.vue
+++ hanfuhui/src/views/Brand/list/index.vue
@@ -35,13 +35,13 @@ export default {
 
 
      created(){
-<<<<<<< HEAD
-        console.log(this.$route.param)
-=======
+        // console.log(this.$route.param)
+
          console.log(this.$route.query)
->>>>>>> a8305e745f5ae0e47df3e5fdeedbb80299214a9d
+         let id=this.$route.query.goodsType
+
          let that=this
-         axios.get("http://192.168.52.90:8080/hanfugou/InChest?goodsType=qx").then(function(res){
+         axios.get("http://192.168.52.90:8080/hanfugou/InChest?goodsType="+id).then(function(res){
              console.log(res.data)
              that._data.store=res.data
              console.log(that._data.store)
